5458eddf0d2cd68b5636e88a3b3bd7fe66a81919
Tran Quang Vinh <vinhtran1997@gmai.com>
2020-05-30 23:49:45 +0700

    code lai

--- src/components/MainBoard.vue
+++ src/components/MainBoard.vue
@@ -1,7 +1,8 @@
 <template>
 <div id="main">
-    <div v-for="(item, index) in scrambledCards" :key="index" class="flipCard" :id="generateId('mycard',index)" :class="generatePair('card',item.id)" @click="checkPair(item.id, index)" style="background-image: url('src/assets/back.png');background-size:cover">
-
+    <div v-for="(item, index) in scrambledCards" :key="index" class="flipCard">
+        <div class="front" :id="generateId('myfront',index)" :class="generatePair('front',item.id)" @click="checkPair(item.id, index)" style="z-index:2;"></div>
+        <div class="back" :id="generateId('myback',index)" :class="generatePair('back',item.id)" @click="checkPair(item.id, index)" style="z-index:1;" :style="{'background-image':'url('+item.path+')'}"></div>
     </div>
 </div>
 </template>
@@ -16,67 +17,66 @@ export default {
         return {
             lstCard: [{
                     id: 1,
-                    path: 'src/assets/image (1).jpg'
                 },
                 {
                     id: 2,
-                    path: 'src/assets/image (2).jpg'
+                    path: 'src/assets/image2.jpg'
                 },
                 {
                     id: 3,
-                    path: 'src/assets/image (3).jpg'
+                    path: 'src/assets/image3.jpg'
                 },
                 {
                     id: 4,
-                    path: 'src/assets/image (4).jpg'
+                    path: 'src/assets/image4.jpg'
                 },
                 {
                     id: 5,
-                    path: 'src/assets/image (5).jpg'
+                    path: 'src/assets/image5.jpg'
                 },
                 {
                     id: 6,
-                    path: 'src/assets/image (6).jpg'
+                    path: 'src/assets/image6.jpg'
                 },
                 {
                     id: 7,
-                    path: 'src/assets/image (7).jpg'
+                    path: 'src/assets/image7.jpg'
                 },
                 {
                     id: 8,
-                    path: 'src/assets/image (8).jpg'
+                    path: 'src/assets/image8.jpg'
                 },
                 {
                     id: 9,
-                    path: 'src/assets/image (9).jpg'
+                    path: 'src/assets/image9.jpg'
                 },
                 {
                     id: 10,
-                    path: 'src/assets/image (10).jpg'
+                    path: 'src/assets/image10.jpg'
                 },
                 {
                     id: 11,
-                    path: 'src/assets/image (11).jpg'
+                    path: 'src/assets/image11.jpg'
                 },
                 {
                     id: 12,
-                    path: 'src/assets/image (12).jpg'
+                    path: 'src/assets/image12.jpg'
                 },
                 {
                     id: 13,
-                    path: 'src/assets/image (13).jpg'
+                    path: 'src/assets/image13.jpg'
                 },
                 {
                     id: 14,
-                    path: 'src/assets/image (14).jpg'
+                    path: 'src/assets/image14.jpg'
                 },
                 {
                     id: 15,
-                    path: 'src/assets/image (15).jpg'
+                    path: 'src/assets/image15.jpg'
                 },
                 {
                     id: 16,
-                    path: 'src/assets/image (16).jpg'
+                    path: 'src/assets/image16.jpg'
                 }
             ],
             scrambledCards: [],
@@ -85,9 +85,10 @@ export default {
         }
     },
     created() {
-        this.lstCard=this.shuffle(this.lstCard);
-        this.scrambledCards = this.lstCard.slice(0,12).concat(this.lstCard.slice(0,12));
+        this.lstCard = this.shuffle(this.lstCard);
+        this.scrambledCards = this.lstCard.slice(0, 12).concat(this.lstCard.slice(0, 12));
         this.scrambledCards = this.shuffle(this.scrambledCards);
+
     },
     methods: {
         shuffle(array) {
@@ -117,10 +118,14 @@ export default {
         },
         checkPair(id, index) {
             //flip
-            var openedCard = document.getElementById("mycard" + index);
-            openedCard.style.transform = 'rotatey(-180deg)';
-            openedCard.style.backgroundImage = "url('" + this.scrambledCards[index].path + "')";
-
+            var openedCardFront = document.getElementById("myfront" + index);
+            var openedCardBack = document.getElementById("myback" + index);
+            if (openedCardFront.style.zIndex == 2) {
+                openedCardFront.style.transform = 'rotatey(-180deg)';
+                openedCardBack.style.transform = 'rotatey(-180deg)';
+                openedCardFront.style.zIndex = 1;
+                openedCardBack.style.zIndex = 2;
+            }
             //logic
             if (this.openedId == 0 && this.openedIndex == -1) {
                 this.openedId = id;
@@ -132,9 +137,12 @@ export default {
                 } else {
                     if (id == this.openedId) {
                         // increase point
-                        var openedCards = document.getElementsByClassName("card" + id);
-                        openedCards[0].style.visibility = 'hidden';
-                        openedCards[1].style.visibility = 'hidden';
+                        var openedCardsFront = document.getElementsByClassName("front" + id);
+                        var openedCardsBack = document.getElementsByClassName("back" + id);
+                        openedCardsFront[0].style.visibility = 'hidden';
+                        openedCardsFront[1].style.visibility = 'hidden';
+                        openedCardsBack[0].style.visibility = 'hidden';
+                        openedCardsBack[1].style.visibility = 'hidden';
                         this.openedId = 0;
                         this.openedIndex = -1;
                         point += 1;
@@ -153,14 +161,20 @@ export default {
                         }
                     } else {
                         // false, close both card
-                        var openedCard1 = document.getElementById("mycard" + index);
-                        var openedCard2 = document.getElementById("mycard" + this.openedIndex);
+                        var openedCardFront1 = document.getElementById("myfront" + index);
+                        var openedCardBack1 = document.getElementById("myback" + index);
+                        var openedCardFront2 = document.getElementById("myfront" + this.openedIndex);
+                        var openedCardBack2 = document.getElementById("myback" + this.openedIndex);
                         setTimeout(function () {
-                            openedCard1.style.transform = 'rotatey(-360deg)';
-                            openedCard1.style.backgroundImage = "url('src/assets/back.png')";
-                            openedCard2.style.transform = 'rotatey(-360deg)';
-                            openedCard2.style.backgroundImage = "url('src/assets/back.png')";
-                        }, 1000);
+                            openedCardFront1.style.transform = 'rotatey(0deg)';
+                            openedCardBack1.style.transform = 'rotatey(0deg)';
+                            openedCardFront1.style.zIndex = 2;
+                            openedCardBack1.style.zIndex = 1;
+                            openedCardFront2.style.transform = 'rotatey(0deg)';
+                            openedCardBack2.style.transform = 'rotatey(0deg)';
+                            openedCardFront2.style.zIndex = 2;
+                            openedCardBack2.style.zIndex = 1;
+                        }, 800);
                         this.openedId = 0;
                         this.openedIndex = -1;
                     }
